0697fda56d76f186c43891edaf6c3b9f5e30e8a6
Muhammad Usman <musmananwar1009@gmail.com>
2021-09-18 13:22:15 +0500

    Update sum-numbers.js

--- sum-numbers.js
+++ sum-numbers.js
@@ -1,3 +1,5 @@
+//Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
+
 const num = Number(prompt("Enter a number"));
 let result = 0;
 for(let i=1;i<=num;i++){
